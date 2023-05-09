8b60a10da833ff8bd6b53db262ce7e0bccf79628
sangwon lee <leeh8911@gmail.com>
2023-05-10 06:00:58 +0900

    update

--- premake5.lua
+++ premake5.lua
@@ -9,3 +9,74 @@
 
     pchheader "gauri_pch.h"
     pchsource "src/gauri_pch.cpp"
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp",
+        "include/**.h",
+    }
+
+    defines
+    {
+        "_CRT_SECURE_NO_WARNINGS",
+        "GLFW_INCLUDE_NONE"
+    }
+
+    includedirs
+    {
+        "src",
+        "include",
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.ImGui}",
+    }
+
+    links
+    {
+        "GLFW",
+        "ImGui",
+        "opengl32.lib"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        defines
+        {
+        }
+
+        links
+        {
+            "%{Library.winSock}",
+            "%{Library.winMM}",
+            "%{Library.WinVersion}",
+            "%{Library.BCrypt}"
+        }
+
+
+	filter "configurations:Debug"
+		defines "GR_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+		links
+		{
+		}
+
+	filter "configurations:Release"
+		defines "GR_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+		}
+
+	filter "configurations:Dist"
+		defines "GR_DIST"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+		}
