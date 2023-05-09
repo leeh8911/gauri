df0b7d163c8aa41d331a2379201ff4072ed66287
sangwon lee <leeh8911@gmail.com>
2023-05-10 05:56:45 +0900

    update

---
+++ premake5.lua
@@ -0,0 +1,11 @@
+﻿project "gauri"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "off"
+
+    targetdir ("${wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("${wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    pchheader "gauri_pch.h"
+    pchsource "src/gauri_pch.cpp"
