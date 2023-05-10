2bd633f18e16385f48e28c57fd67007aefe91b72
sangwon lee <leeh8911@gmail.com>
2023-05-11 06:00:26 +0900

    update premake files

--- premake5.lua
+++ premake5.lua
@@ -4,8 +4,8 @@
     cppdialect "C++20"
     staticruntime "off"
 
-    targetdir ("${wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("${wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
     pchheader "gauri_pch.h"
     pchsource "src/gauri_pch.cpp"
@@ -16,17 +16,13 @@
         "src/**.cpp",
         "include/**.h",
     }
-
-    defines
-    {
-        "_CRT_SECURE_NO_WARNINGS",
-        "GLFW_INCLUDE_NONE"
-    }
+    removefiles {"src/test/**.cpp", "src/test/**.h"}
 
     includedirs
     {
         "src",
         "include",
+        "3rdparty/spdlog/include",
         "%{IncludeDir.GLFW}",
         "%{IncludeDir.ImGui}",
     }
@@ -36,6 +32,77 @@
         "GLFW",
         "ImGui",
         "opengl32.lib"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        defines
+        {
+        }
+
+        links
+        {
+            "%{Library.winSock}",
+            "%{Library.winMM}",
+            "%{Library.WinVersion}",
+            "%{Library.BCrypt}"
+        }
+
+
+	filter "configurations:Debug"
+		defines "GR_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+		links
+		{
+		}
+
+	filter "configurations:Release"
+		defines "GR_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+		}
+
+	filter "configurations:Dist"
+		defines "GR_DIST"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+		}
+
+
+project "gauri-test"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    pchheader "test/gauri_test_pch.h"
+    pchsource "test/gauri_test_pch.cpp"
+
+    files
+    {
+        "test/**.cpp",
+    }
+
+    includedirs
+    {
+        "include",
+    }
+
+    links
+    {
+        "gtest"
     }
 
     filter "system:windows"
