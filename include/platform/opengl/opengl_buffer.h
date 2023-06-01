2cde4698ceeb5b63bb6ff2c11bd0e031fb54893a
sangwon lee <leeh8911@gmail.com>
2023-06-02 05:52:52 +0900

    update vertex buffer layout

--- include/platform/opengl/opengl_buffer.h
+++ include/platform/opengl/opengl_buffer.h
@@ -14,9 +14,20 @@ class OpenGLVertexBuffer : public VertexBuffer
     void Bind() const override;
     void Unbind() const override;
 
+    void SetLayout(const BufferLayout &layout) override
+    {
+        m_Layout = layout;
+    }
+    const BufferLayout &GetLayout() const override
+    {
+        return m_Layout;
+    }
+
   private:
     uint32_t m_RendererID;
+    BufferLayout m_Layout;
 };
+
 class OpenGLIndexBuffer : public IndexBuffer
 {
   public:
