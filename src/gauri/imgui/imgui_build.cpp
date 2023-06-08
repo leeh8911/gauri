a368f9de51f22d753034c25773543fc46e40137a
leeh8911 <54252269+leeh8911@users.noreply.github.com>
2023-06-08 21:32:05 +0000

    Deploying to gh-pages from @ leeh8911/gauri@5025c6778f427e0b801357d16610413870ee5bbc 🚀

--- src/gauri/imgui/imgui_build.cpp
+++
@@ -1,7 +0,0 @@
-#include "grpch.h"
-
-// clang-format off
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include "backends/imgui_impl_opengl3.cpp"
-#include "backends/imgui_impl_glfw.cpp"
-// clang-format on
--- src/gauri/renderer/renderer_api.cpp
+++
@@ -1,8 +0,0 @@
-#include "grpch.h"
-
-#include "gauri/renderer/renderer_api.h"
-
-namespace gauri
-{
-RendererAPI::API RendererAPI::s_API = RendererAPI::API::OpenGL;
-} // namespace gauri
