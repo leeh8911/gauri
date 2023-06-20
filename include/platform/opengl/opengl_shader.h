4e2726bf6941e20e039099fd781a68c2557f4091
sangwon lee <leeh8911@gmail.com>
2023-06-20 20:31:27 +0900

    update renderer2d texture update

--- include/platform/opengl/opengl_shader.h
+++ include/platform/opengl/opengl_shader.h
@@ -21,6 +21,7 @@ class OpenGLShader : public Shader
     void Bind() const override;
     void Unbind() const override;
 
+    void SetInt(const std::string &name, int value) override;
     void SetFloat3(const std::string &name, const glm::vec3 &value) override;
     void SetFloat4(const std::string &name, const glm::vec4 &value) override;
     void SetMat4(const std::string &name, const glm::mat4 &value) override;
