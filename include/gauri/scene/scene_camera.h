b83065d1ee83f3702bfa913823ee44ecedfd1eeb
sangwon lee <leeh8911@gmail.com>
2023-07-16 06:58:37 +0900

    update camera system and fix bug

--- include/gauri/scene/scene_camera.h
+++ include/gauri/scene/scene_camera.h
@@ -14,11 +14,21 @@ class SceneCamera : public Camera
 
     void SetViewportSize(uint32_t width, uint32_t height);
 
+    float GetOrthographicSize() const
+    {
+        return m_OrthographicSize;
+    }
+    void SetOrthographicSize(float size)
+    {
+        m_OrthographicSize = size;
+        RecalculateProjection();
+    }
+
   private:
     float m_OrthographicSize = 10.0f;
     float m_OrthographicNear = -1.0f, m_OrthographicFar = 1.0f;
 
-    float m_AspectRatio = 1.0f;
+    float m_AspectRatio = 0.0f;
 
     void RecalculateProjection();
 };
